bb47c3609dc24ef4f74e2b3fd2eeb80b8ecc74c0
jinger02 <maryjane.galindo@gmail.com>
2023-09-20 07:24:18 -0400

    add input argument

---
+++ element.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo -e "\n\n~~~~ Periodic Table ~~~~\n\n"
+
+if [[ -z $1 ]]
+then
+    echo -e "\nPlease provide an element as an argument.\n"
+    exit
+fi
+
--- manipulation.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
-
-TYPES=$($PSQL "SELECT type FROM properties")
-echo $TYPES
