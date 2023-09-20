f9dd361f37371ddb88890e5f2115a8324d960e89
jinger02 <maryjane.galindo@gmail.com>
2023-09-20 07:54:20 -0400

    test: first argument is a number

--- element.sh
+++ element.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
+
 echo -e "\n\n~~~~ Periodic Table ~~~~\n\n"
 
 if [[ -z $1 ]]
@@ -8,3 +10,12 @@ then
     exit
 fi
 
+if [[ $1 =~ ^[1-9]+$ ]]
+then
+    ELEMENT=$($PSQL "SELECT atomic_number,name, symbol,type,atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) WHERE atomic_number=$1")
+fi
+
+echo $ELEMENT | while IFS=" | " read atomic_num name symbol type mass mp bp
+do
+  echo -e "The element with atomic number $atomic_num is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
+done
